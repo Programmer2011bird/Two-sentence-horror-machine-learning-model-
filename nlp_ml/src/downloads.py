6923cf1b6e34a18107b34757a8297872ff98ed6c
Programmer2011bird <maysamshaker7@gmail.com>
2024-08-30 18:00:51 +0330

    nltk downloads here - adjust as need

--- nlp_ml/src/downloads.py
+++ nlp_ml/src/downloads.py
@@ -1,8 +1,10 @@
 import nltk
 
 def main():
-    # nltk.download("punkt_tab")
-    nltk.download("stopwords")
+    nltk.download("punkt_tab", "j:/projects/pythonprojects/26.two_sentence_horror_ml/nlp_ml/Lib")
+    nltk.download("stopwords", "j:/projects/pythonprojects/26.two_sentence_horror_ml/nlp_ml/Lib")
+    nltk.download('averaged_perceptron_tagger_eng', "j:/projects/pythonprojects/26.two_sentence_horror_ml/nlp_ml/Lib")
+    nltk.download('wordnet', "j:/projects/pythonprojects/26.two_sentence_horror_ml/nlp_ml/Lib")
 
 if __name__ == "__main__":
     main()
